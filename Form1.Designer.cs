5986736d283c4c37ca4e66ed570bc69aa24d21e5
erkancyln <32850546+erkancyln@users.noreply.github.com>
2019-08-19 02:37:04 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace YazLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(436, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(757, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(243, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Heigth :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(26, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Format : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(238, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Speed : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4:4:4",
+            "4:2:2",
+            "4:2:0"});
+            this.comboBox1.Location = new System.Drawing.Point(81, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0.25",
+            "0.5",
+            "1.0",
+            "1.5",
+            "2.0"});
+            this.comboBox2.Location = new System.Drawing.Point(293, 203);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(153, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(200, 257);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 20);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Save Images";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 398);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "YUV (Raw Data) ÇÖZÜCÜ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace YazLab3
+{
+    public partial class Form1 : Form
+    {
+
+        byte[] array;
+        int width , heigth , frame = 0;
+        byte[] yArray;
+        Color[] color;
+        string file = "";
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+        int time = 0,sayac = 0, imageSayac = 1;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if(String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(comboBox1.Text) || String.IsNullOrEmpty(comboBox2.Text)) {
+                MessageBox.Show("Lütfen Boş Yerleri Doldurunuz !!! ");
+            }
+
+
+            else
+            {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            
+                DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+                if (result == DialogResult.OK) // Test result.
+                {
+                    string file = openFileDialog1.FileName;
+                    try
+                    {
+                        string text = File.ReadAllText(file);
+                    
+                        array = File.ReadAllBytes(file);
+                    }
+                    catch (IOException)
+                    {
+                    }
+                }
+
+
+                width = Convert.ToInt32(textBox1.Text);
+                heigth = Convert.ToInt32(textBox2.Text);
+
+                int size = array.Length;
+
+                yArray = new byte[size];
+                color = new Color[size];
+
+
+                Console.WriteLine("girdi");
+
+                string a = comboBox2.SelectedItem.ToString();
+                Console.WriteLine(Convert.ToInt32(1000 / Convert.ToDouble(a)));
+    
+                timer1.Interval = Convert.ToInt32(1000 / Convert.ToDouble(a)) ;
+                timer1.Enabled = true;
+                
+   
+            
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            if(comboBox1.SelectedItem.ToString() == "4:4:4"){_444format();}
+            if (comboBox1.SelectedItem.ToString() == "4:2:2") { _422format(); }
+            if (comboBox1.SelectedItem.ToString() == "4:2:0") { _420format(); }
+
+        }
+
+        void _444format()
+        {
+            sayac++;
+
+            
+            frame = array.Length / (width * heigth * 3);
+
+            Console.WriteLine(frame);
+            for (int i = time; i < (width * heigth) + time; i++)
+            {
+                yArray[i] = array[i];
+                color[i] = Color.FromArgb(yArray[i], yArray[i], yArray[i]);
+            }
+            Bitmap myBitmap = new Bitmap(width, heigth);
+
+            int a = time;
+
+            for (int i = 0; i < heigth; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    myBitmap.SetPixel(j, i, color[a]);
+                    a++;
+                }
+            }
+
+            pictureBox1.Image = myBitmap;
+
+            if (checkBox1.Checked)
+            {
+                myBitmap.Save(@"C:\Users\PASAVAT\Desktop\Images\image" + imageSayac + ".bmp");
+                imageSayac++;
+            }
+
+            if (sayac < frame) {
+                time += width * heigth * 3;
+            }
+            else
+            {
+                timer1.Stop();
+                time = 0;
+                sayac = 0;
+                pictureBox1.Image = null;
+            }
+            
+        }
+
+        void _422format()
+        {
+            sayac++;
+
+
+            frame = array.Length / (width * heigth * 2);
+
+            Console.WriteLine(frame);
+            for (int i = time; i < (width * heigth) + time; i++)
+            {
+                yArray[i] = array[i];
+                color[i] = Color.FromArgb(yArray[i], yArray[i], yArray[i]);
+            }
+            Bitmap myBitmap = new Bitmap(width, heigth);
+
+            int a = time;
+
+            for (int i = 0; i < heigth; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    myBitmap.SetPixel(j, i, color[a]);
+                    a++;
+                }
+            }
+
+            pictureBox1.Image = myBitmap;
+
+            if (checkBox1.Checked)
+            {
+                myBitmap.Save(@"C:\Users\PASAVAT\Desktop\Images\image" + imageSayac + ".bmp");
+                imageSayac++;
+            }
+
+            if (sayac < frame)
+            {
+                time += width * heigth * 2;
+            }
+            else
+            {
+                timer1.Stop();
+                time = 0;
+                sayac = 0;
+                pictureBox1.Image = null;
+            }
+
+        }
+
+        void _420format()
+        {
+            sayac++;
+
+
+            frame = array.Length / Convert.ToInt32(width * heigth * 1.5);
+
+            Console.WriteLine(frame);
+            for (int i = time; i < (width * heigth) + time; i++)
+            {
+                yArray[i] = array[i];
+                color[i] = Color.FromArgb(yArray[i], yArray[i], yArray[i]);
+            }
+            Bitmap myBitmap = new Bitmap(width, heigth);
+
+            int a = time;
+
+            for (int i = 0; i < heigth; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    myBitmap.SetPixel(j, i, color[a]);
+                    a++;
+                }
+            }
+
+            pictureBox1.Image = myBitmap;
+
+            if (checkBox1.Checked)
+            {
+                myBitmap.Save(@"C:\Users\PASAVAT\Desktop\Images\image" + imageSayac + ".bmp");
+                imageSayac++;
+            }
+
+            if (sayac < frame)
+            {
+                time += Convert.ToInt32(width * heigth * 1.5);
+            }
+            else
+            {
+                timer1.Stop();
+                time = 0;
+                sayac = 0;
+                pictureBox1.Image = null;
+            }
+
+        }
+    }
+}
